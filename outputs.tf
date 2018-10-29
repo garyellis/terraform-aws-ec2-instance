6cd8e6e525a34192116fbb09e4952ba54de68cf9
gary <gary@rancher.com>
2018-10-29 14:43:39 -0700

    Add public_ips output and fix private_ips attribute

--- outputs.tf
+++ outputs.tf
@@ -3,5 +3,9 @@ output "aws_instance_ids" {
 }
 
 output "aws_instance_private_ips" {
-  value = "${split(",", join(",", aws_instance.instance_with_user_data.*.id, aws_instance.instance_with_user_data_base64.*.private_ip))}"
+  value = "${split(",", join(",", aws_instance.instance_with_user_data.*.private_ip, aws_instance.instance_with_user_data_base64.*.private_ip))}"
+}
+
+output "aws_instance_public_ips" {
+  value = "${split(",", join(",", aws_instance.instance_with_user_data.*.public_ip, aws_instance.instance_with_user_data_base64.*.public_ip))}"
 }
