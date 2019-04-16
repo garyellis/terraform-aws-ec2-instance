bf4e42a24b8a8e366aec6b25fca701f9ef0c5171
gary ellis <gary.luis.ellis@gmail.com>
2019-04-15 20:13:33 -0700

    add instance private ip output

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,7 @@ output "aws_instance_private_ips" {
 output "aws_instance_public_ips" {
   value = "${split(",", join(",", aws_instance.instance_with_user_data.*.public_ip, aws_instance.instance_with_user_data_base64.*.public_ip))}"
 }
+
+output "aws_instance_private_dns" {
+  value = "${split(",", join(",", aws_instance.instance_with_user_data.*.private_dns, aws_instance.instance_with_user_data_base64.*.private_dns))}"
+}
